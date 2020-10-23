bd1bd9405aa420b70e98e043daa5447e15a73a86
didi-0 <toothbrush666@163.com>
2020-10-23 20:26:12 +0800

    Initial commit

---
+++ shjava0420/src/main/java/com/example/shjava0420/Shjava0420Application.java
@@ -0,0 +1,13 @@
+package com.example.shjava0420;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shjava0420Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shjava0420Application.class, args);
+    }
+
+}
---
+++ shjava0420/src/test/java/com/example/shjava0420/Shjava0420ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shjava0420;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shjava0420ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
